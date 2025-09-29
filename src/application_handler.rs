d0a9c50faa06ab505864a3d633842afd4b5d5e4c
Alessandro <48774358+AlessandroScrem@users.noreply.github.com>
2025-09-29 21:57:31 +0200

    Update application_handler.rs

--- src/application_handler.rs
+++ src/application_handler.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+òuse std::sync::Arc;
 
 use crate::assets::texture_manager::TextureManager;
 use crate::input::Input;
@@ -17,27 +17,22 @@ impl ApplicationHandler for App {
             "App resumed after being paused for {} ms",
             timer.elapsed().as_millis()
         );
-        
-        // Resize to monitor max size
-        if let Some(monitor) = window.current_monitor() {
-            let screen_size = monitor.size(); // dimensione dello schermo in px
-            let safe_x = min(screen_size.width, self.size.width);
-            let safe_y = min(screen_size.height, self.size.height);
-            self.width = safe_x;
-            self.height = safe_y;
-        }
 
-        let size = self.size;
         let attributes = Window::default_attributes()
-            .with_inner_size(size)
+            .with_inner_size(self.size)
             .with_title(format!("App"));
         let window = std::sync::Arc::new(event_loop.create_window(attributes).unwrap());
 
         // Dopo aver creato la finestra, la possiamo centrare
         if let Some(monitor) = window.current_monitor() {
             let screen_size = monitor.size(); // dimensione dello schermo in px
-            let x = (screen_size.width as u32 - size.width) as f32 / 2.0;
-            let y = (screen_size.height as u32 - size.height)as f32  / 2.0;
+            let safe_width = min(screen_size.width, self.size.width);
+            let safe_height = min(screen_size.height, self.size.height);
+            let x = (screen_size.width as u32 - safe_width) as f32 / 2.0;
+            let y = (screen_size.height as u32 - safe_height)as f32  / 2.0;
+            if let Some(size) = window.request_inner_size(winit::dpi::PhisicalPosition::new(safe_width, safe_height)){
+                self.size = size;
+            }
             window.set_outer_position(winit::dpi::PhysicalPosition::new(x, y));
         }
 
